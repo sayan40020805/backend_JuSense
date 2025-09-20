4acd9e1b6411d74adb1b59b260900c455f87e509
sayan <ssayanmjhi204@gmail.com>
2025-09-20 22:12:59 +0530

    check

--- cleanupVotes.js
+++ cleanupVotes.js
@@ -1,10 +1,16 @@
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/quickpolls')
 const mongoose = require('mongoose');
 const Vote = require('./models/Vote');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/quickpolls')
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/quickpolls';
+console.log('Connecting to:', uri);
+
+mongoose.connect(uri)
   .then(async () => {
     const result = await Vote.deleteMany({ guestId: null });
-    console.log('Deleted votes:', result.deletedCount);
+    console.log('Deleted votes with guestId: null:', result.deletedCount);
+    const stillExists = await Vote.find({ guestId: null });
+    console.log('Votes with guestId: null remaining:', stillExists.length);
     mongoose.disconnect();
   })
   .catch(err => {
